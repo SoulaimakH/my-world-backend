6891b26a73e0e68fcbdff324683a5b8f5c691910
HadilHelali <helalihadil4@gmail.com>
2023-01-17 11:04:27 +0100

    changing the return token function

--- src/typorem/config.ts
+++ src/typorem/config.ts
@@ -14,17 +14,10 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       host: 'localhost',
       port: 3306,
       username: 'root',
-<<<<<<< HEAD
       password: 'admin',
       database: 'spacy',
-      entities: [User],
-      autoLoadEntities: true,
-=======
-      password: '',
-      database: 'spacy',
       entities: [User , Article],
->>>>>>> 8721693924eb9082569ec9d5a4222a4e5e94659c
-      synchronize: true,
+      autoLoadEntities: true,
     };
   }
 }
